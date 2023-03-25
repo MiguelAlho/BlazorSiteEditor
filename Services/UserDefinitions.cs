47555836ff72c15f2f40308677d86b426edb28b2
Miguel Alho <malho@letsgetchecked.com>
2023-03-25 20:40:13 +0000

    add hugo url on local machine to definitions

--- Services/UserDefinitions.cs
+++ Services/UserDefinitions.cs
@@ -2,10 +2,16 @@ using System.Text.Json;
 
 namespace MiguelAlho.BlazorSiteEditor.Services;
 
+/// <summary>
+/// Carries machine / User specific settings. I use two different machines (Windows and Mac) so paths can be different on each,
+/// but I want to be able to use and edit, when possible the editor on those.   
+/// </summary>
 class UserDefinitionsService
 {
     private static string FilePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/blazorsiteeditoruserdefinitions.json";
 
+    public static bool Exists() => File.Exists(FilePath);
+    
     public static void Save(UserDefinitions definitions)
     {
         Console.WriteLine(FilePath);
@@ -22,10 +28,9 @@ class UserDefinitionsService
         return JsonSerializer.Deserialize<UserDefinitions>(File.ReadAllText(FilePath)) ?? new();
     }
 
-    public static bool Exists() => File.Exists(FilePath);
-
-
+    
     public static string SiteRepositoryPath => Load().SiteRepositoryPath;
+    
     public static string BookmarksFolderPath {
         get{
             var definitions = Load();
@@ -41,6 +46,8 @@ class UserDefinitionsService
             return $"{definitions.SiteRepositoryPath}{definitions.BookmarkImagesBaseFolderPath}";
         }
     }
+    
+    public static string LocalHugoUrl => Load().LocalHugoUrl;
 }
 
 public class UserDefinitions
@@ -49,4 +56,6 @@ public class UserDefinitions
     public string BookmarksFolderPath {get;set;} = "/content/bookmarks";
     public string PostsFolderPath {get;set;} = "/content/posts";
     public string BookmarkImagesBaseFolderPath { get; set; } = "/static/bookmarks";
+    
+    public string LocalHugoUrl {get;set;} = "http://localhost:1313";
 }
